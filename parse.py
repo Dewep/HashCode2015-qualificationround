2f58c9f1daa81a8c16bc0534f13c6119e6f20083
maigre_a <dewep.net@gmail.com>
2015-03-12 19:20:11 +0100

    parse base

--- parse.py
+++ parse.py
@@ -1,3 +1,27 @@
 #!/usr/bin/env python3
 
 R, S, U, P, M = input().split()
+R = int(R)
+S = int(S)
+U = int(U)
+P = int(P)
+M = int(M)
+
+indispo = []
+servers = []
+map = list()
+
+for i in range(0, U):
+    indispo.append(input().split())
+
+for i in range(0, M):
+    servers.append(input().split())
+
+print(R)
+print(S)
+print(U)
+print(P)
+print(M)
+
+print(indispo)
+print(servers)
